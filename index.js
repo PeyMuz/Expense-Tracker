63905519f313bc02da10331d195244c54fdb1c8e
PeyMuz <kbantog1@gmail.com>
2026-09-18 08:07:52 +0800

    updated js

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ const transactionForm = document.getElementById("transaction-form");
 const des = document.getElementById("description");
 const amountEl = document.getElementById("amount");
 
-let transactions = JSON.parse(localStorage.getItem("transactions")) || []
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+
+transactionForm.addEventListener("submit", addTransaction)
+
+function addTransaction (e) {
+    e.preventDefault();   //so it doesn't refresh the page
+
+    // get form values
+    const description = des.value.trim();
+    const amount = parseFloat(amountEl.value);
+    
+    transactions.push({ //push uses to add value at the end of the array
+      id:Date.now(),
+      description,
+      amount
+    });
+
+    //to convert javascript object and array inro formatted strings
+    localStorage.setItem("transactions",JSON.stringify(transactions));
+
+    updateTransactionList();
+    updateSummary();
+
+    transactionForm.reset();
+
+}
